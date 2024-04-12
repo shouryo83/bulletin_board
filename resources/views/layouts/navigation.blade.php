a1ed6d47c9fc0226566d4328e878216aa0e93b15
shouryo83 <mikisirex@gmail.com>
2024-04-12 19:05:52 +0900

    スレッドの実装

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('threads') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
@@ -20,6 +20,7 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
+                @auth
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
@@ -50,6 +51,15 @@
                         </form>
                     </x-slot>
                 </x-dropdown>
+                @endauth
+                
+                @guest
+                   <div class="text-sm text-gray-700 dark:text-gray-500">
+                        <a href="{{ route('login') }}">{{ __('Log in') }}</a>&emsp;
+                        <a href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </div>
+                @endguest
+                    
             </div>
 
             <!-- Hamburger -->
@@ -73,6 +83,7 @@
         </div>
 
         <!-- Responsive Settings Options -->
+        @auth
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
@@ -96,5 +107,6 @@
                 </form>
             </div>
         </div>
+        @endauth
     </div>
 </nav>
