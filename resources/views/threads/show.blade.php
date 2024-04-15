12c2e52d6c80e0877ba79a7b5b0e980d2569f0b7
shouryo83 <mikisirex@gmail.com>
2024-04-15 12:33:23 +0900

    コメント機能の実装

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,20 +3,33 @@
         <div class="flex items-center gap-x-4">
             <a href="{{ route('threads') }}">
                 <img src="/left_arrow.png" width="30" height="30" alt="">
-            </a>
+            </a>&emsp;
             <div class="max-w-4x1">
                 <h1 class="font-semibold text-xl text-gray-800 leading-tight">{{ $thread->title }}</h1>
             </div>
         </div>
     </x-slot>
     
-    <div class="pt-6 max-w-4x1 mx-auto grid bg-white mt-4">
-        @if ($comments->count())
-            @foreach ($comments as $comment)
-            <x-comment-card :comment="$comment" />
-            @endforeach
-        @else
-            No comments
-        @endif
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="p-6 bg-white border-b border-gray-200">
+            @if ($comments->count())
+                @foreach ($comments as $comment)
+                <x-comment-card :comment="$comment" />
+                @endforeach
+            @else
+                No comments
+            @endif
+            
+            <form action="{{ route('comments.store', $thread) }}" method="POST" class="m-4">
+                @csrf
+                <label for="body">{{ __('Comment') }}</label>
+                <textarea name="body" id="body" cols="30" rows="4" class="w-full rounded-lg border-2 bg-gray-100 @error('comment') border-red-500 @enderror"></textarea>
+                <div class="mt-4">
+                                <button type="submit" class"bg-bule-500 rounded font-medium px-4 py-2 text-white">{{ __('Submit') }}</button>
+                </div>
+            </form>
+        </div>
+      </div>
     </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ThreadController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,7 +32,8 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/threads', [ThreadController::class, 'index'])->name('threads');
 Route::get('/threads/create', [ThreadController::class, 'create'])->name('threads.create');
-Route::post('/threads/create', [ThreadController::class, 'store']);
+Route::post('/threads/create', [ThreadController::class, 'store'])->name('threads,store');
 Route::get('/threads/{thread}', [ThreadController::class, 'show'])->name('threads.show');
+Route::post('/threads/{thread}/comments', [CommentController::class, 'store'])->name('comments.store');
 
 require __DIR__.'/auth.php';
